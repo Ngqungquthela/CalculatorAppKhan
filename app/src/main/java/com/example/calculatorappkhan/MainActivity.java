9df521a001153385f50f7c932cd9f151d119fc8d
Ngqungquthela <ziyan.khan214@gmail.com>
2023-09-06 12:54:57 -0500

    Button functionality started

--- app/src/main/java/com/example/calculatorappkhan/MainActivity.java
+++ app/src/main/java/com/example/calculatorappkhan/MainActivity.java
@@ -3,7 +3,10 @@ package com.example.calculatorappkhan;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,7 +16,24 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    public void onClick(View v){
-        
+    public void clear(View v){
+        TextView tvNumbers = findViewById(R.id.textViewNumbers);
+        tvNumbers.setText("0");
+    }
+
+    public void numClick(View v){
+        // Code for how to get text from a pressed button, found here:
+        // https://stackoverflow.com/questions/5620772/get-text-from-pressed-button
+        Button b = (Button)v;
+        String buttonText = b.getText().toString();
+
+        TextView tvNumbers = findViewById(R.id.textViewNumbers);
+
+        if(tvNumbers.toString().equals("0")){
+            tvNumbers.setText(buttonText);
+        }
+        else {
+            tvNumbers.setText(tvNumbers + buttonText);
+        }
     }
 }
