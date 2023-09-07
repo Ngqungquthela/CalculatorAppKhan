efe0e11138736a335d28a191014a5b0ec04f60f8
Ngqungquthela <ziyan.khan214@gmail.com>
2023-09-07 12:55:23 -0500

    Most of coding done, still crashing

--- app/src/main/java/com/example/calculatorappkhan/MainActivity.java
+++ app/src/main/java/com/example/calculatorappkhan/MainActivity.java
@@ -9,16 +9,15 @@ import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    String eqn = "";
+    String first = "";
+    String second = "";
+    TextView tvNumbers = findViewById(R.id.textViewNumbers);
+    boolean doneOperation = false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-
-    public void clear(View v){
-        TextView tvNumbers = findViewById(R.id.textViewNumbers);
-        tvNumbers.setText("0");
     }
 
     public void numClick(View v){
@@ -27,13 +26,53 @@ public class MainActivity extends AppCompatActivity {
         Button b = (Button)v;
         String buttonText = b.getText().toString();
 
-        TextView tvNumbers = findViewById(R.id.textViewNumbers);
-
-        if(tvNumbers.toString().equals("0")){
-            tvNumbers.setText(buttonText);
+        if(buttonText.equals("CLEAR")){
+            eqn = "";
+            tvNumbers.setText("0");
         }
-        else {
-            tvNumbers.setText(tvNumbers + buttonText);
+        else if(!doneOperation){
+            first += buttonText;
+            tvNumbers.setText(first);
+        }
+        else{
+            second += buttonText;
+            tvNumbers.setText(second);
+        }
+    }
+
+    public void equation(View v){
+        // Code for how to get text from a pressed button, found here:
+        // https://stackoverflow.com/questions/5620772/get-text-from-pressed-button
+        Button b = (Button)v;
+        String buttonText = b.getText().toString();
+        String operation = "";
+
+        if(!buttonText.equals("=")){
+            operation = buttonText;
+            doneOperation = true;
+        }
+        else{
+            Double part1 = Double.parseDouble(first);
+            Double part2 = Double.parseDouble(second);
+            Double result = 0.0;
+
+            if(operation.equals("/")){
+                result = (part1 / part2);
+            }
+            else if(operation.equals("*")){
+                result = (part1 * part2);
+            }
+            else if(operation.equals("-")){
+                result = (part1 - part2);
+            }
+            else if(operation.equals("+")){
+                result = (part1 + part2);
+            }
+            else if(operation.equals("%")){
+                result = (part1 % part2);
+            }
+
+            tvNumbers.setText(result.toString());
         }
     }
 }
