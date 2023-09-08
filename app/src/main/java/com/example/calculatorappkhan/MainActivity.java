2243e23ef80adc9c31c1029250a5883459a0b34d
Ngqungquthela <ziyan.khan214@gmail.com>
2023-09-08 12:55:23 -0500

    Functional and working

--- app/src/main/java/com/example/calculatorappkhan/MainActivity.java
+++ app/src/main/java/com/example/calculatorappkhan/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.calculatorappkhan;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -9,15 +10,23 @@ import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    String eqn = "";
-    String first = "";
-    String second = "";
-    TextView tvNumbers = findViewById(R.id.textViewNumbers);
-    boolean doneOperation = false;
+    String eqnText;
+    String first;
+    String operation;
+    String second;
+    TextView tvNumbers;
+    boolean doneOperation;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        eqnText = "";
+        first = "";
+        operation = "";
+        second = "";
+        tvNumbers = findViewById(R.id.textViewNumbers);
+        doneOperation = false;
     }
 
     public void numClick(View v){
@@ -27,52 +36,63 @@ public class MainActivity extends AppCompatActivity {
         String buttonText = b.getText().toString();
 
         if(buttonText.equals("CLEAR")){
-            eqn = "";
+            eqnText = "";
+            first = "";
+            second = "";
+            operation = "";
+            doneOperation = false;
             tvNumbers.setText("0");
+        }
+        else if(buttonText.equals("=")){
+            equation(first, operation, second);
+        }
+        else if(buttonText.equals("/") || buttonText.equals("*") || buttonText.equals("-") || buttonText.equals("+") || buttonText.equals("%")){
+            operation = buttonText;
+            eqnText += (" " + operation + " ");
+            tvNumbers.setText(eqnText);
+            doneOperation = true;
+            Log.i("khan", eqnText);
         }
         else if(!doneOperation){
             first += buttonText;
-            tvNumbers.setText(first);
+            eqnText += buttonText;
+            tvNumbers.setText(eqnText);
+            Log.i("khan", eqnText);
         }
         else{
             second += buttonText;
-            tvNumbers.setText(second);
+            eqnText += buttonText;
+            tvNumbers.setText(eqnText);
+            Log.i("khan", eqnText);
         }
     }
 
-    public void equation(View v){
-        // Code for how to get text from a pressed button, found here:
-        // https://stackoverflow.com/questions/5620772/get-text-from-pressed-button
-        Button b = (Button)v;
-        String buttonText = b.getText().toString();
-        String operation = "";
+    public void equation(String one, String op, String two){
+        Double part1 = Double.parseDouble(one);
+        Double part2 = Double.parseDouble(two);
+        Double result;
 
-        if(!buttonText.equals("=")){
-            operation = buttonText;
-            doneOperation = true;
+        if(op.equals("/")){
+            result = (part1 / part2);
+        }
+        else if(op.equals("*")){
+            result = (part1 * part2);
+        }
+        else if(op.equals("-")){
+            result = (part1 - part2);
+        }
+        else if(op.equals("+")){
+            result = (part1 + part2);
         }
         else{
-            Double part1 = Double.parseDouble(first);
-            Double part2 = Double.parseDouble(second);
-            Double result = 0.0;
-
-            if(operation.equals("/")){
-                result = (part1 / part2);
-            }
-            else if(operation.equals("*")){
-                result = (part1 * part2);
-            }
-            else if(operation.equals("-")){
-                result = (part1 - part2);
-            }
-            else if(operation.equals("+")){
-                result = (part1 + part2);
-            }
-            else if(operation.equals("%")){
-                result = (part1 % part2);
-            }
-
-            tvNumbers.setText(result.toString());
+            result = (part1 % part2);
         }
+        result = Math.floor(result * 1000) / 1000;
+        tvNumbers.setText(result.toString());
+        eqnText = "";
+        first = "";
+        second = "";
+        operation = "";
+        doneOperation = false;
     }
 }
